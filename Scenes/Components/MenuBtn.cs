2a846dbde9b0375dc2908a775b2353be66aa4e84
Explogame <explogamer43@gmail.com>
2025-02-04 18:44:46 +0100

    add transition animations and fix some bugs

--- Scenes/Components/MenuBtn.cs
+++ Scenes/Components/MenuBtn.cs
@@ -4,16 +4,15 @@ using System;
 
 public partial class MenuBtn : TextureButton
 {
-	private AnimationPlayer _transitions;
+	
 	
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		_transitions = GetNode<AnimationPlayer>("Transitions");
+		
 		
 		Pressed += () => {
-			_transitions.Play("fade_out");
 			Doodle.ChangeScene(this, "res://Scenes/Menu/Menu.tscn");
 		};
 	}
